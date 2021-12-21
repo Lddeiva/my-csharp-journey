096d61327b30f686539eeb3cf4efc8765ad490ea
Deivasigamani Duraisamy <61077834+Lddeiva@users.noreply.github.com>
2021-12-21 23:27:34 +0530

    02 - Create Program.cs

---
+++ Code/02 - Print an unassigned integer/Program.cs
@@ -0,0 +1,9 @@
+using System;
+class Numbers
+{
+    static void Main()
+    {
+        int number1;
+        Console.WriteLine(number1); // throws error CS0165: Use of unassigned local variable 'number1'
+    }
+}
