d3a07c6aec8981e3c50e8bf71277bf90b3b5d8c8
Deivasigamani Duraisamy <61077834+Lddeiva@users.noreply.github.com>
2021-12-24 14:46:38 +0530

    Add 05 - Print the sum of two integers

---
+++ Code/05 - Print the sum of two integers/Program.cs
@@ -0,0 +1,13 @@
+//Program.cs
+using System;
+class Numbers
+{
+    static void Main()
+    {
+        int number1 = 7, number2 = 8, result;
+        Console.WriteLine(number1 + number2); // prints 15
+        
+        result = number1 + number2;
+        Console.WriteLine(result); // prints 15
+    }
+}
